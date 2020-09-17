2c5af4d35c224988106046e8681babe5e9ac1b10
Agusta Katrin <agustak20@ru.is>
2020-09-17 09:49:16 +0000

    First commit

---
+++ TileTraveller/tiletraveller.py
@@ -0,0 +1,4 @@
+# Player starts in tile [1,1]
+# Player selects moves (n/N, e/E, s/S, w/W) and the program should print the player's travel options
+# If the move is possible, that is, there is an open wall in the direction, the direction is written as a possible travel direction
+# If the player enter invalid direction, should print an error message
