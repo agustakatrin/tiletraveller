053677592abf1c633bbb478e2c3d77c9ad98f241
Ásta Sól <astab19@ru.is>
2020-09-17 12:39:17 +0000

    Fifth commit

--- TileTraveller/tiletraveller.py
+++ TileTraveller/tiletraveller.py
@@ -4,7 +4,7 @@
 
 x_start, y_start = 1, 1
 
-north, south, east, west = 1, -1, 1, -1
+n, s, e, w = 1, -1, 1, -1
 
 def start_tile(x_hnit, y_hnit):
     victory = False
@@ -13,19 +13,19 @@ def start_tile(x_hnit, y_hnit):
         valid_input = "n"
     
     elif x_hnit == 1 and y_hnit == 2:      #1,2
-        dir_map = "(S)outh, (E)ast, (W)est"
-        valid_input = "s", "e", "w"
+        dir_map = "(N)orth or (E)ast or (S)outh"
+        valid_input = "s", "e", "n"
 
     elif x_hnit == 1 and y_hnit == 3:      #1,3
-        dir_map = "(S)outh, (E)ast"
+        dir_map = "(E)ast or (S)outh"
         valid_input = "s", "e"
     
     elif x_hnit == 2 and y_hnit == 3:      #2,3
-        dir_map = "(W)est, (E)ast"
+        dir_map = "(E)ast or (W)est"
         valid_input = "w", "e"
 
     elif x_hnit == 2 and y_hnit == 2:      #2,2
-        dir_map = "(W)est, (S)outh"
+        dir_map = "(S)outh or (W)est"
         valid_input = "w", "s"
     
     elif x_hnit == 2 and y_hnit == 1:      #2,1
@@ -38,11 +38,11 @@ def start_tile(x_hnit, y_hnit):
         victory = True
 
     elif x_hnit == 3 and y_hnit == 2:      #3,2
-        dir_map = "(N)orth, (S)outh"
+        dir_map = "(N)orth or (S)outh"
         valid_input = "n", "s"
 
     elif x_hnit == 3 and y_hnit == 3:      #3,3
-        dir_map = "(W)est, (S)outh"
+        dir_map = "(S)outh or W)est"
         valid_input = "w", "s"
     return dir_map, valid_input, victory
 
@@ -50,35 +50,47 @@ def dir_invalid():
     print("Not a valid direction!")
 
 
-def play(my_dir, valid_dir, x, y):
+def mover(my_dir, valid_dir, x, y):
     
     if(my_dir in valid_dir):
 
         if my_dir == "s" or my_dir == "n":
-            y += int(my_dir)
+            y += eval(my_dir)
         
         else: 
-            x += int(my_dir)
+            x += eval(my_dir)
     
     else:
         dir_invalid
     
     return x, y
 
-def input(valid_dir):
+def input_letter(valid_dir):
 
-    dir = input("Direction: ").lower()
+    direction = input("Direction: ").lower()
 
-    while dir not in valid_dir:
+    while direction not in valid_dir:
         dir_invalid()
-        dir = input("Direction: ").lower()
-    return dir
+        direction = input("Direction: ").lower()
+    return direction
 
 def guide(possible_dir, victory):
     
-    if victory = True:
-        print("Victory!")
+    if victory != True:
+        print("You can travel: " + possible_dir)
     else:
-        print("You can travel: ", possible_dir)
+        print("Victory!")
 
 
+while 1:
+    dir_map, valid_input, victory = start_tile(x_start, y_start)
+
+    guide(dir_map, victory)
+    if victory == True:
+        break
+
+    direction = input_letter(valid_input)
+
+    x_start, y_start = mover(direction, valid_input, x_start, y_start)
+
+    
